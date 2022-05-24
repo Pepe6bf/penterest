e46e63e42d449954ea4224464c6ae54862962df0
Pepe6bf <pepe6bf@gmail.com>
2022-05-24 23:51:13 +0900

    프로젝트 생성

---
+++ src/main/java/site/penterest/penterest/PenterestApplication.java
@@ -0,0 +1,13 @@
+package site.penterest.penterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/penterest/penterest/PenterestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.penterest.penterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
