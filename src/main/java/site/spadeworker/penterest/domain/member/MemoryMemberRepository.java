59681ceffc302f11559129da08af01a5253b3961
Pepe6bf <pepe6bf@gmail.com>
2022-05-26 21:06:04 +0900

    MemoryMemberRepository - 회원 생성/수정 로직 작성

--- src/main/java/site/spadeworker/penterest/domain/member/MemoryMemberRepository.java
+++ src/main/java/site/spadeworker/penterest/domain/member/MemoryMemberRepository.java
@@ -1,26 +1,47 @@
 package site.spadeworker.penterest.domain.member;
 
-import java.util.List;
-import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.*;
+
+@Slf4j
 public class MemoryMemberRepository implements MemberRepository {
+
+    // Member 저장소
+    private static Map<Long, Member> store = new HashMap<>();
+    // Member idx
+    private static long sequence = 0L;
+
     @Override
     public Member save(Member member) {
-        return null;
+        member.setId(++sequence);
+        log.info("save : member={}", member);
+        store.put(member.getId(), member);
+        return member;
     }
 
     @Override
     public Member findById(Long id) {
-        return null;
+        return store.get(id);
     }
 
     @Override
     public Optional<Member> findByLoginId(String loginId) {
+        List<Member> all = findAll();
+        for (Member m : all) {
+            if (m.getLoginId().equals(loginId)) {
+                return Optional.of(m);
+            }
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
