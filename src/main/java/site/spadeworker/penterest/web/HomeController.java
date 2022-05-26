a3b4d24e155f96ba0911d243e5cad63d909354e4
Pepe6bf <pepe6bf@gmail.com>
2022-05-26 20:21:11 +0900

    home page 생성 및 컨트롤러 테스트

---
+++ src/main/java/site/spadeworker/penterest/web/HomeController.java
@@ -0,0 +1,13 @@
+package site.spadeworker.penterest.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
