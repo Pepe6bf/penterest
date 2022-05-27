e77ae213273ac0ca092f23a0c889e7c7c6cc9a78
Pepe6bf <pepe6bf@gmail.com>
2022-05-27 15:30:14 +0900

    feat: Http 요청/응답 로깅 기능 추가 (Spring Interceptor 기반)

--- src/main/java/site/spadeworker/penterest/web/WebConfig.java
+++ src/main/java/site/spadeworker/penterest/web/WebConfig.java
@@ -3,12 +3,23 @@ package site.spadeworker.penterest.web;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.spadeworker.penterest.web.filter.LoginCheckFilter;
+import site.spadeworker.penterest.web.interceptor.LogInterceptor;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
 
     @Bean
     public FilterRegistrationBean loginCheckFilter() {
